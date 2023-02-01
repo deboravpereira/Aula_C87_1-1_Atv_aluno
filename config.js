1a94fd388a2899bdd4e354133d1cad2df9574602
Debora Pereira <deboravpereira@hotmail.com>
2023-02-01 17:38:35 -0300

    Atividade do Aluno com correções

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1_FFk2zolnQKyCQ_7hblcdFCv6t0KWtY",
+  authDomain: "app-narrador-historias.firebaseapp.com",
+  databaseURL: "https://app-narrador-historias-default-rtdb.firebaseio.com",
+  projectId: "app-narrador-historias",
+  storageBucket: "app-narrador-historias.appspot.com",
+  messagingSenderId: "541972808172",
+  appId: "1:541972808172:web:c8d3f62f9518a4f890c0c9",
+  measurementId: "G-SNSNC731SC"
+};
